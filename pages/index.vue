81096f343f0100687987eaabbbf68104ae4de586
Alex Froemling <alexander.froemling@gmail.com>
2021-05-25 13:58:11 +0200

    14 Uhr

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,19 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        indy
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+ <div>
+  <div class="bar">
+   <h1>Indiana Jones Restaurant</h1>
+    <div class="side-left"></div>
+    <nav>
+      <ul class="container">
+        <li><a href="#home" class="menu">HOME</a></li>
+        <li><a href="#menu" class="menu">MENÜ</a></li>
+        <li><a href="#about" class="menu">ÜBER UNS</a></li>
+        <li><a href="#contact" class="menu">KONTAKT</a></li>
+      </ul>
+    </nav>
+    <div class="side-right"></div>
   </div>
+ </div>  
 </template>
 
 <script>
