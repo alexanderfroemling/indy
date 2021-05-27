f82164081f67f101e648aaa806d165efdd1288b1
Alex Froemling <alexander.froemling@gmail.com>
2021-05-27 11:48:08 +0200

    11:48

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,25 +1,43 @@
 <template>
  <div>
   <header class="header">
-      <h1 class="title">Indy Food</h1>
-      <ul>
-          <li>
-              <nuxt-link to='/'>HOME</nuxt-link>
-              <nuxt-link to='/menu'>MENU</nuxt-link>
-              <nuxt-link to='/guestbook'>GÄSTEBUCH</nuxt-link>
-              <nuxt-link to='/about'>ABOUT</nuxt-link>
-              <nuxt-link to='/contact'>KONTAKT</nuxt-link>
-          </li>
-      </ul>
+    <h1 class="title">Indy Food</h1>
+    <ul>
+        <li>
+        <nuxt-link to='/'>HOME</nuxt-link>
+        <nuxt-link to='/menu'>MENU</nuxt-link>
+        <nuxt-link to='/guestbook'>GÄSTEBUCH</nuxt-link>
+        <nuxt-link to='/about'>ABOUT</nuxt-link>
+        <nuxt-link to='/contact'>KONTAKT</nuxt-link>
+        </li>
+    </ul>
+    <div class="header_cart">
+        <button @click="showCart = !showCart">
+        <i class="fas fa-shopping-cart"></i>
+        </button>
+        <span class="total-quantity">{{ totalQuantity }}</span>
+        <div v-if="showCart" class="cart-dropdown">
+            <ul class="cart-dropdown__list">
+                <li
+                v-for="product in cart"
+                :key="product.id"
+                >
+                {{ product.name }} ({{ product.quantity}})
+                </li>
+            </ul>
+        </div>
+    </div> 
   </header>
  </div>
   
 </template>
 
 <script>
+
 export default {
     name: 'AppHeader'
 }
+
 </script>
 
 <style>
@@ -58,6 +76,58 @@ export default {
   background-color: rgb(0, 183, 255);
   opacity: 90%;
   border: white solid 3px;
+}
+
+.headercart {
+    position: relative;
+}
+
+button {
+    background: none;
+    border: 0;
+    color: white;
+    cursor: pointer;
+}
+
+i {
+font-size: 2rem;
+}
+
+.total-quantity {
+align-items: center;
+background: lightblue;
+border-radius: 50%;
+display: flex;
+font-weight: bold;
+height: 2rem;
+justify-content: center;
+padding: 0.5rem;
+position: absolute;
+right: -10px;
+top: -10px;
+width: 2rem;
+}
+
+.cart-dropdown {
+background: white;
+border: 1px solid lightgray;
+border-radius: 10px;
+box-shadow: 0 0 2px rgba(0, 0, 0, 0.2);
+color: #333;
+font-size: 1.3rem;
+overflow: auto;
+padding: 0 1rem;
+position: absolute;
+right: 0;
+width: 12rem;
+}
+
+.cart-dropdown__list {
+    list-style: none;
+} 
+
+li {
+   margin: 1rem 0;
 }
 
 </style>
