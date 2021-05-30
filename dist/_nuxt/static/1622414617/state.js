48a23a2134fd0e1dc75d7d4fa5569156d32ba161
Alex Froemling <alexander.froemling@gmail.com>
2021-05-31 00:49:21 +0200

    schon wieder

--- dist/_nuxt/static/1622414617/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Findy\u002F_nuxt\u002Fstatic\u002F1622414617",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Findy\u002F",assetsPath:"\u002Findy\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ dist/_nuxt/static/1622414946/menu/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Findy\u002Fdist\u002F_nuxt\u002Fstatic\u002F1622414946",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmenu",config:{_app:{basePath:"\u002Findy\u002Fdist\u002F",assetsPath:"\u002Findy\u002Fdist\u002F_nuxt\u002F",cdnURL:a}}}}(null));
